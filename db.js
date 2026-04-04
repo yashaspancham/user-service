6bfc61b68403a5e790f79b419fe986d32fc196e6
Yashas <121193460+yashaspancham@users.noreply.github.com>
2026-04-04 10:04:28 +0530

    Initialize MySQL connection in db.js
    Set up MySQL connection with environment variables.

--- db.js
+++ db.js
@@ -1,1 +1,16 @@
-this is db
+const mysql = require("mysql2");
+
+// Create a connection pool
+const db = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD, // ✅ matches .env
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+// Export the pool
+module.exports = { pool };
